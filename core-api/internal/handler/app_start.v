42457d76623de4f188c39bfad21a3b6c32621145
Jengro <avey777@outlook.com>
2025-03-30 17:27:48 +0800

    Use config to set web port

--- core-api/internal/handler/app_start.v
+++ core-api/internal/handler/app_start.v
@@ -2,16 +2,18 @@ module handler
 
 import veb
 import log
+import internal.config {toml_load}
 
 
 pub fn new_app() {
 	log.debug('${@METHOD}  ${@MOD}.${@FILE_LINE}')
+	doc := toml_load()
 
 	mut app := &App{} // 实例化 App 结构体 并返回指针
 	register_handlers(mut app) // veb.Controller  使用路由控制器 | handler/register_routes.v
 	cores_middleware(mut app)
 
-	mut port := 9009  //config.set_web_port()
+	mut port := doc.value('web.port').int()
 	veb.run_at[App, Context](mut app, host: '', port: port, family: .ip6, timeout_in_seconds: 30) or {
 		panic(err)
 	}
