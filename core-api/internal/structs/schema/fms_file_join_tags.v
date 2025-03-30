909893e131f2566d6d1674769fea95927fd535fc
Jengro <avey777@outlook.com>
2025-03-30 13:42:07 +0800

    Rename file and struct to FmsFileJoinTag

--- core-api/internal/structs/schema/fms_file_join_tags.v
+++ core-api/internal/structs/schema/fms_file_join_tags.v
@@ -1,8 +1,8 @@
 module schema
 
 // 文件与标签关联表
-@[table: 'fms_file_tag_files']
-pub struct FmsFileTagFile {
+@[table: 'fms_file_join_tags']
+pub struct FmsFileJoinTag {
 pub:
 	file_tag_id string @[primary; sql: 'file_tag_id'; sql_type: 'CHAR(36)'; zcomments: '标签ID']
 	file_id     string @[primary; sql: 'file_id'; sql_type: 'CHAR(36)'; zcomments: '文件UUID']
