d5a9954a86cf7185bcb8a4e57bd81646d891df51
Jengro <avey777@outlook.com>
2025-04-03 14:13:47 +0800

    Update orm_m.v

--- example/orm_m.v
+++ example/orm_m.v
@@ -2,30 +2,29 @@ module main
 
 import db.mysql
 import log
+import time
 
 fn main() {
 	log.debug('${@METHOD}  ${@MOD}.${@FILE_LINE}')
 
 	db := db_mysql() or { panic('failed to connect to database') }
-	create_db(db)
-	// sql db {
-	// 	create table User
-	// } or { panic(err) }
+	query_db(db)
 	log.info('sys_users init Success')
 }
 
 @[table: 'sys_users']
 struct User {
 pub:
-	id        string  @[immutable; primary; sql: 'id'; sql_type: 'VARCHAR(255)'; unique]
-	home_path ?string @[default: 'dashboard'; omitempty; sql_type: 'VARCHAR(64)'] // success
-	// home_path ?string @[default: '0212654'; omitempty; sql_type: 'VARCHAR(64)']  //failed
+	id           string  @[immutable; primary; sql: 'id'; sql_type: 'VARCHAR(255)'; unique]
+	created_at    ?time.Time @[omitempty; sql_type: 'TIMESTAMP' ]
+	updated_at    ?time.Time @[omitempty; sql_type: 'TIMESTAMP']
 }
 
-fn create_db(db mysql.DB) {
-	sql db {
-		create table User
+fn query_db(db mysql.DB) {
+  mut query := sql db {
+		select table User
 	} or { panic(err) }
+	dump(query)
 }
 
 fn db_mysql() !mysql.DB {
