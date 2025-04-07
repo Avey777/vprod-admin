8b61c512c3e2c7458ee33ad355aac4473571f3ef
Jengro <avey777@outlook.com>
2025-04-07 14:32:48 +0800

    Refactor route middleware for base, admin, user

--- core-api/internal/handler/routes.v
+++ core-api/internal/handler/routes.v
@@ -10,21 +10,21 @@ import internal.middleware { cores_middleware, logger_middleware }
 pub fn register_handlers(mut app App) {
 	log.debug('${@METHOD}  ${@MOD}.${@FILE_LINE}')
 
-	mut base_app := &Base{}
-	mut admin_app := &Admin{}
-	mut user_app := &User{}
-
 	app.use(cores_middleware())
-	admin_app.use(cores_middleware())
-
 	app.use(handler: logger_middleware)
-	base_app.use(handler: logger_middleware)
-	admin_app.use(handler: logger_middleware)
-	user_app.use(handler: logger_middleware)
 
-	// register the controllers the same way as how we start a veb app
+	mut base_app := &Base{}
+	base_app.use(handler: logger_middleware)
 	app.register_controller[Base, Context]('/base', mut base_app) or { log.error('${err}') }
+
+	mut admin_app := &Admin{}
+	admin_app.use(cores_middleware())
+	admin_app.use(handler: logger_middleware)
 	app.register_controller[Admin, Context]('/admin', mut admin_app) or { log.error('${err}') }
+
+	mut user_app := &User{}
+	user_app.use(cores_middleware())
+	user_app.use(handler: logger_middleware)
 	app.register_controller[User, Context]('/admin/user', mut user_app) or { log.error('${err}') }
 
 	// app.register_controller[Member, Context]('/member', mut &Member{}) or { log.error('${err}') }
