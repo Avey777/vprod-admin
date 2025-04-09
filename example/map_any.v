bd2f8fe93266ff6eee84b43770bfd83971ec1b59
Jengro <avey777@outlook.com>
2025-04-09 14:06:15 +0800

    Add SQLite integration and user JSON encoding

--- example/map_any.v
+++ example/map_any.v
@@ -1,20 +1,78 @@
+// module main
+
+// type Any = string | int | f64 | bool | []map[string]Any
+
+// fn main() {
+// 	// 创建 map[string]any 的切片
+// 	mut data_list := []map[string]Any{}
+
+// 	mut item := map[string]Any{}
+// 	item['name'] = 'name'
+// 	item['age'] = 1
+// 	data_list << item
+// 	dump(data_list)
+
+// 	mut result := map[string]Any{}
+// 	result['account'] = 123
+// 	result['data'] = data_list
+
+// 	dump(result)
+// }
+
 module main
 
-type Any = string | int | f64 | bool | []map[string]Any
+import db.sqlite
+import time
+import json
+
+@[table: 'sys_users']
+struct Users {
+	id         string     @[immutable; primary; sql: 'id'; sql_type: 'VARCHAR(255)'; unique]
+	name       ?string    @[immutable; sql: 'name'; sql_type: 'VARCHAR(255)'; unique]
+	created_at ?time.Time @[omitempty; sql_type: 'TIMESTAMP']
+	updated_at ?time.Time @[omitempty; sql_type: 'TIMESTAMP']
+}
+
+type Any = string | int | []string | []int | bool | []map[string]Any | time.Time
 
 fn main() {
-	// 创建 map[string]any 的切片
-	mut data_list := []map[string]Any{}
+	mut db := sqlite.connect(':memory:')!
+	defer { db.close() or {} }
 
-	mut item := map[string]Any{}
-	item['name'] = 'name'
-	item['age'] = 1
-	data_list << item
-	dump(data_list)
+	sql db {
+		create table Users
+	}!
+	user1 := Users{
+		id:         '123'
+		name:       'name1'
+		created_at: time.now()
+		// updated_at defaults to none
+	}
+	user2 := Users{
+		id: 'abc'
+		// name:       'name2'
+		created_at: time.now()
+		updated_at: time.now()
+	}
 
-	mut result := map[string]Any{}
-	result['account'] = 123
-	result['data'] = data_list
+	sql db {
+		insert user1 into Users
+		insert user2 into Users
+	}!
 
-	dump(result)
+	data_all := sql db {
+		select from Users
+	}!
+	// dump(data_all)
+
+	mut result := []map[string]Any{}
+	for raw in data_all {
+		mut data := map[string]Any{}
+		data['id'] = raw.id
+		data['name'] = raw.name or { '' }
+		data['updatedAt'] = raw.updated_at or { time.Time{} }.format_ss()
+		result << data
+	}
+	// dump(result)
+	dump(json.encode(result))
 }
