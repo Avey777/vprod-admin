faad4ded4bec45cee610eefc59f6f224a998f1a2
Jengro <avey777@outlook.com>
2025-04-16 11:23:06 +0800

    Update temp.v

--- example/temp.v
+++ example/temp.v
@@ -8,58 +8,77 @@ import orm
 @[table: 'sys_users']
 struct User {
 pub:
-	id         string     @[immutable; primary; sql: 'id'; sql_type: 'VARCHAR(255)'; unique]
-	name       string    @[immutable; sql: 'username'; sql_type: 'VARCHAR(255)'; unique]
-	created_at ?time.Time @[omitempty; sql_type: 'TIMESTAMP']
-	updated_at time.Time  @[omitempty; sql_type: 'TIMESTAMP']
+    id         string     @[immutable; primary; sql: 'id'; sql_type: 'VARCHAR(255)'; unique]
+    name       string     @[immutable; sql: 'username'; sql_type: 'VARCHAR(255)'; unique]
+    created_at ?time.Time @[omitempty; sql_type: 'TIMESTAMP']
+    updated_at time.Time  @[omitempty; sql_type: 'TIMESTAMP']
+}
+
+struct UserVo {
+pub:
+    id         string
+    name       string
+    created_at time.Time
+    updated_at time.Time
 }
 
 struct Context {
-	veb.Context
+    veb.Context
 }
 
 struct App {
-	veb.Middleware[Context]
+    veb.Middleware[Context]
 }
 
 fn main() {
-	port := 9009
-	mut app := &App{}
-	veb.run[App, Context](mut app, port)
+    port := 9009
+    mut app := &App{}
+    veb.run[App, Context](mut app, port)
 }
 
 @['/'; get]
-fn (app &App) index(mut ctx Context) !veb.Result {
-	mut pool := db_pool()
-	defer { pool.close() }
+fn (app &App) index(mut ctx Context) veb.Result {
+    mut pool := db_pool()
+    defer { pool.close() }
 
-	mut pb := pool.acquire()!
-	defer { pool.release(pb) }
-	dump(pb)
+    mut pb := pool.acquire() or { panic('456') }
+    defer { pool.release(pb) }
+    dump(pb)
 
-	mut result0 := sql pb {
-		select from User
-	}!
-	dump(result0)
+    mut result0 := sql pb {
+        select from User
+    } or { panic('345') }
+    dump(result0)
 
-	mut qb := orm.new_query[User](pb)
-	result := qb.select()!.query()!
-	dump(result)
+    mut qb := orm.new_query[User](pb)
+    result := qb.select() or { panic('123') }.query() or { panic('234') }
+    dump(result)
 
-	return ctx.json(result)
+    mut tmps := []UserVo{}
+    for r in result {
+        tmp := UserVo{
+            id:         r.id
+            name:       r.name
+            created_at: r.created_at or { time.unix(0) }
+            updated_at: r.updated_at
+        }
+        tmps << tmp
+    }
+
+    return ctx.json(tmps)
 }
 
 fn db_pool() mysql.ConnectionPool {
-	config := mysql.Config{
-		host:     'mysql2.sqlpub.com'
-		port:     3307
-		username: 'vcore_test'
-		password: 'wfo8wS7CylT0qIMg'
-		dbname:   'vcore_test'
-	}
+    config := mysql.Config{
+        host:     'mysql2.sqlpub.com'
+        port:     3307
+        username: 'vcore_test'
+        password: 'wfo8wS7CylT0qIMg'
+        dbname:   'vcore_test'
+    }
 
-	mut pool := mysql.new_connection_pool(config, 10) or { panic('Failed to create pool: ${err}') }
-	dump(pool)
+    mut pool := mysql.new_connection_pool(config, 10) or { panic('Failed to create pool: ${err}') }
+    dump(pool)
 
-	return pool
+    return pool
 }
