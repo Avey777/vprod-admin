c2ba5d87493dcb50b1071419dab429c4015ac4ac
Jengro <avey777@outlook.com>
2025-05-09 18:21:35 +0800

    jwt

--- example/jwt.v
+++ example/jwt.v
@@ -4,7 +4,10 @@ import crypto.hmac
 import crypto.sha256
 import encoding.base64
 import json
+import x.json2
 import time
+
+
 
 
 //JWT 头部固定使用HS256算法
@@ -16,9 +19,11 @@ const header = base64.url_encode_str(json.encode({
 //生成令牌
 fn generate(secret string,payload map[string]Any) string {
   playload_64 := base64.url_encode_str(json.encode(payload))
+
   message := '${header}.${playload_64}'
   signature := hmac.new(secret.bytes(),message.bytes(),sha256.sum,0)
-  return '${header}.${playload_64}.${base64.url_encode_str(signature.bytestr())}'
+  base64_signature := base64.url_encode_str(signature.bytestr())
+  return '${header}.${playload_64}.${base64_signature}'
 }
 
 
@@ -38,8 +43,6 @@ fn main(){
     // 自定义业务字段 (Custom Claims)
     "name": "Jengro",               // 用户姓名
     "roles": ["admin", "editor"],   // 用户角色
-    "email": "user@example.com",    // 用户邮箱
-    "avatar": "https://example.com/avatar.png", // 头像地址
     "status": "active",             // 用户状态
     "meta": {                       // 嵌套对象
        "login_ip": "192.168.1.100",
@@ -48,8 +51,9 @@ fn main(){
   }
 
   token := generate(secret,payload)
-  println(token)
+  // println(token)
 
+  // verify(secret,token)
   bj := verify(secret,token)
   println(bj)
 }
@@ -57,17 +61,56 @@ fn main(){
 
 // 验证令牌
 fn verify(secret string, token string) bool {
-    parts := token.split('.')
-    if parts.len != 3 {
-        return false
-    }
-    message := '${parts[0]}.${parts[1]}'
-    signature := hmac.new(secret.bytes(), message.bytes(), sha256.sum, 0)
-    expected_sig := base64.url_encode_str(signature.bytestr())
-    return parts[2] == expected_sig
+
+  // 验证 token 长度
+  parts := token.split('.')
+  if parts.len != 3 {
+      return false
+  }
+
+  // 验证 Header
+
+  // 验证签名
+  message := '${parts[0]}.${parts[1]}'
+  signature := hmac.new(secret.bytes(), message.bytes(), sha256.sum, 0)
+  expected_sig := base64.url_encode_str(signature.bytestr())
+  if parts[2] != expected_sig{
+    return false
+  }
+
+  // 验证时间有效性
+  // dump(parts[1])
+  payload_json := base64.url_decode_str(parts[1])
+  payload := json2.decode[Payload](payload_json.str()) or {return false}
+  dump(payload)
+  now := time.now().unix()
+  if now > payload.exp || now < payload.nbf {
+      return false
+  }
+
+  return true
 }
 
+struct Header {
+    alg string
+    typ string
+}
 
+struct Payload {
+    // 标准声明
+    iss string
+    sub string
+    aud []string
+    exp i64
+    nbf i64
+    iat i64
+    jti string
+    // 自定义声明
+    name   string
+    roles  []string
+    status string
+    meta   map[string]string
+}
 
 type F64 = f64
 type I64 = i64
