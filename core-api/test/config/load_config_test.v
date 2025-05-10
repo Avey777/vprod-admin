bb93630e059c69f91f80503d3e07b5293f27dde5
Jengro <avey777@outlook.com>
2025-05-10 16:43:50 +0800

    Update jwt.v

--- core-api/test/config/load_config_test.v
+++ core-api/test/config/load_config_test.v
@@ -4,5 +4,5 @@ import internal.config
 
 fn test_toml_load() {
 	// doc_toml := config.read_toml()!
-	assert typeof(config.toml_load()!).name == 'toml.Doc'
+	assert typeof(config.toml_load()).name == 'toml.Doc'
 }
--- core-api/test/config/toml_check_test.v
+++
@@ -1,9 +0,0 @@
-module config
-
-// import internal.config
-
-// fn test_set_web_port() {
-// 	web_port := config.set_web_port()
-// 	assert typeof(web_port).name == 'int'
-// 	assert web_port > 1000 && web_port < 65535
-// }
