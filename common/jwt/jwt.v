1efb5f88ebae87cd474db986c7cd8fe23d5a475d
Jengro <avey777@outlook.com>
2025-05-10 16:32:16 +0800

    Update jwt.v

--- common/jwt/jwt.v
+++ common/jwt/jwt.v
@@ -31,7 +31,6 @@ fn jwt_verify(secret string, token string) bool {
 	if parts.len != 3 {
 		return false
 	}
-
 	// 验证签名
 	message := '${parts[0]}.${parts[1]}'
 	signature := hmac.new(secret.bytes(), message.bytes(), sha256.sum, 64)
@@ -40,11 +39,10 @@ fn jwt_verify(secret string, token string) bool {
 		return false
 	}
 	// 验证时间有效性
-	payload_json := base64.url_decode_str(parts[1])
-	payload := json2.decode[JWT_Payload](payload_json.str()) or { return false }
-	dump(payload)
+	payload_base64 := base64.url_decode_str(parts[1])
+	payload_json := json2.decode[JWT_Payload](payload_base64.str()) or { return false }
 	now := time.now().unix()
-	if now > payload.exp || now < payload.nbf {
+	if now > payload_json.exp || now < payload_json.nbf {
 		return false
 	}
 
