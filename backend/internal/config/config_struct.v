3b425d780792de056d7db2532cc60b8a3499b9c2
Jengro <avey777@outlook.com>
2025-05-16 16:46:34 +0800

    Update load_config_test.v

--- backend/internal/config/config_struct.v
+++ backend/internal/config/config_struct.v
@@ -1,64 +1,64 @@
 module config
 
-// import time
+import time
 
-// // MiddlewaresConf is the config of middlewares.
-// pub struct MddlewaresConf {
-// pub:
-// 	trace      bool @['default=true'; json: 'Trace']      // Enable trace middleware
-// 	log        bool @['default=true'; json: 'Log']        // 日志中间件
-// 	prometheus bool @['default=true'; json: 'Prometheus'] // Enable prometheus middleware
-// 	max_conns  bool @['default=true'; json: 'MaxConns']   // Enable max connections middleware
-// 	breaker    bool @['default=true'; json: 'Breaker']    // Enable circuit breaker middleware
-// 	shedding   bool @['default=true'; json: 'Shedding']   // Enable shedding middleware
-// 	timeout    bool @['default=true'; json: 'Timeout']    // 超时中间件
-// 	recover    bool @['default=true'; json: 'Recover']    // Enable recover middleware
-// 	metrics    bool @['default=true'; json: 'Metrics']    // Enable metrics middleware
-// 	max_bytes  bool @['default=true'; json: 'MaxBytes']   // Enable max bytes middleware
-// 	gunzip     bool @['default=true'; json: 'Gunzip']     // Enable gunzip middleware
-// 	i18n       bool @['default=true'; json: 'I18n']       // Enable i18n middleware
-// 	tenant     bool @['default=false'; json: 'Tenant']    // Enable tenant middleware
-// 	client_ip  bool @['default=false'; json: 'ClientIP']  // Enable client IP middleware
-// }
+// MiddlewaresConf is the config of middlewares.
+pub struct MddlewaresConf {
+pub:
+	trace      bool @['default=true'; json: 'Trace']      // Enable trace middleware
+	log        bool @['default=true'; json: 'Log']        // 日志中间件
+	prometheus bool @['default=true'; json: 'Prometheus'] // Enable prometheus middleware
+	max_conns  bool @['default=true'; json: 'MaxConns']   // Enable max connections middleware
+	breaker    bool @['default=true'; json: 'Breaker']    // Enable circuit breaker middleware
+	shedding   bool @['default=true'; json: 'Shedding']   // Enable shedding middleware
+	timeout    bool @['default=true'; json: 'Timeout']    // 超时中间件
+	recover    bool @['default=true'; json: 'Recover']    // Enable recover middleware
+	metrics    bool @['default=true'; json: 'Metrics']    // Enable metrics middleware
+	max_bytes  bool @['default=true'; json: 'MaxBytes']   // Enable max bytes middleware
+	gunzip     bool @['default=true'; json: 'Gunzip']     // Enable gunzip middleware
+	i18n       bool @['default=true'; json: 'I18n']       // Enable i18n middleware
+	tenant     bool @['default=false'; json: 'Tenant']    // Enable tenant middleware
+	client_ip  bool @['default=false'; json: 'ClientIP']  // Enable client IP middleware
+}
 
-// // A PrivateKeyConf is a private key config.
-// struct PrivateKeyConf {
-// 	fingerprint string
-// 	key_file    string
-// }
+// A PrivateKeyConf is a private key config.
+struct PrivateKeyConf {
+	fingerprint string
+	key_file    string
+}
 
-// // // A SignatureConf is a signature config.
-// // struct SignatureConf {
-// // 	strict       bool          @['default=false'; json: 'Strict'] // Enable strict signature validation
-// // 	expiry       time.Duration @['default=1h'; json: 'Expiry']    // Set the duration for signature expiry
-// // 	private_keys []PrivateKeyConf // Configure private keys for signature validation
-// // }
+// A SignatureConf is a signature config.
+struct SignatureConf {
+	strict       bool          @['default=false'; json: 'Strict'] // Enable strict signature validation
+	expiry       time.Duration @['default=1h'; json: 'Expiry']    // Set the duration for signature expiry
+	private_keys []PrivateKeyConf // Configure private keys for signature validation
+}
 
-// // AuthConf is a JWT config
-// struct AuthConf {
-// 	access_secret string @[json: 'optional,env=AUTH_SECRET'] // Configure access secret for JWT authentication
-// 	access_expire i64    @[json: 'optional,env=AUTH_EXPIRE'] // Configure access expiration time for JWT authentication
-// }
+// AuthConf is a JWT config
+struct AuthConf {
+	access_secret string @[json: 'optional,env=AUTH_SECRET'] // Configure access secret for JWT authentication
+	access_expire i64    @[json: 'optional,env=AUTH_EXPIRE'] // Configure access expiration time for JWT authentication
+}
 
-// /* A RestConf is a http service config.
-// Why not name it as Conf, because we need to consider usage like:
-//  type Config struct {
-//     zrpc.RpcConf
-//     rest.RestConf
-//  }
-// if with the name Conf, there will be two Conf inside Config */
-// struct RestConf {
-// 	service.ServiceConf
-// 	host               string        @[json: 'default=0.0.0.0,env=API_HOST']      // Configure host address for HTTP service
-// 	port               int           @[json: 'env=API_PORT']                      // Configure port number for HTTP service
-// 	certFile           string        @[json: 'optional,env=API_CERT_FILE']        // Configure certificate file for HTTPS service
-// 	keyFile            string        @[json: 'optional,env=API_KEY_FILE']         // Configure key file for HTTPS service
-// 	verbose            bool          @[json: 'optional,env=API_VERBOSE']          // Configure verbose mode for HTTP service
-// 	mmax_conns         int           @[json: 'default=10000,env=API_MAX_CONNS']   // Configure maximum number of concurrent connections for HTTP service
-// 	max_bytes          int64         @[json: 'default=1048576,env=API_MAX_BYTES'] // Configure maximum request body size for HTTP service
-// 	timeout            int64         @[json: 'default=3000,env=API_TIMEOUT']      // [milliseconds] Configure timeout for HTTP service
-// 	cpu_threshold      int64         @[json: 'default=900,range=[0:1000)']
-// 	signature          SignatureConf @[json: 'optional,env=API_SIGNATURE']
-// 	middlewares        MiddlewaresConf // There are default values for all the items in Middlewares
-// 	trace_ignore_paths []string @[json: 'optional'] // TraceIgnorePaths is paths blacklist for trace middleware
-// }
+/* A RestConf is a http service config.
+Why not name it as Conf, because we need to consider usage like:
+ type Config struct {
+    zrpc.RpcConf
+    rest.RestConf
+ }
+if with the name Conf, there will be two Conf inside Config */
+struct RestConf {
+	// service.ServiceConf
+	host          string        @[json: 'default=0.0.0.0,env=API_HOST']      // Configure host address for HTTP service
+	port          int           @[json: 'env=API_PORT']                      // Configure port number for HTTP service
+	cert_file     string        @[json: 'optional,env=API_CERT_FILE']        // Configure certificate file for HTTPS service
+	key_file      string        @[json: 'optional,env=API_KEY_FILE']         // Configure key file for HTTPS service
+	verbose       bool          @[json: 'optional,env=API_VERBOSE']          // Configure verbose mode for HTTP service
+	mmax_conns    int           @[json: 'default=10000,env=API_MAX_CONNS']   // Configure maximum number of concurrent connections for HTTP service
+	max_bytes     i64           @[json: 'default=1048576,env=API_MAX_BYTES'] // Configure maximum request body size for HTTP service
+	timeout       i64           @[json: 'default=3000,env=API_TIMEOUT']      // [milliseconds] Configure timeout for HTTP service
+	cpu_threshold i64           @[json: 'default=900,range=[0:1000)']
+	signature     SignatureConf @[json: 'optional,env=API_SIGNATURE']
+	// middlewares        MiddlewaresConf // There are default values for all the items in Middlewares
+	trace_ignore_paths []string @[json: 'optional'] // TraceIgnorePaths is paths blacklist for trace middleware
+}
--- backend/internal/svc/service_context.v
+++ backend/internal/svc/service_context.v
@@ -1,44 +1,53 @@
+/*
+暂时未使用
+svc SeriveContext  服务上下文容器：聚合服务运行所需的所有核心依赖
+*/
 module svc
 
-// import 	c-core-api.internal.config
-// import (
-// "express-delivery/edcores/edcoresclient"
+	// "github.com/mojocn/base64Captcha"
+	// "github.com/redis/go-redis/v9"
+	// "github.com/suyuan32/simple-admin-common/i18n"
+	// "github.com/suyuan32/simple-admin-common/utils/captcha"
+	// "github.com/suyuan32/simple-admin-job/jobclient"
+	// "github.com/suyuan32/simple-admin-message-center/mcmsclient"
 
-// "gitea.com/Tos/ed-driver/internal/config"
-// i18n2 "gitea.com/Tos/ed-driver/internal/i18n"
-// "gitea.com/Tos/ed-driver/internal/middleware"
+	import internal.config
+	// i18n2 "github.com/suyuan32/simple-admin-core/api/internal/i18n"
+	// "github.com/suyuan32/simple-admin-core/api/internal/middleware"
+	// "github.com/suyuan32/simple-admin-core/rpc/coreclient"
 
-// "github.com/suyuan32/simple-admin-common/i18n"
-// "github.com/suyuan32/simple-admin-core/rpc/coreclient"
+	// "github.com/casbin/casbin/v2"
+	// "github.com/zeromicro/go-zero/rest"
+	// "github.com/zeromicro/go-zero/zrpc"
 
-// "github.com/casbin/casbin/v2"
-// "github.com/zeromicro/go-zero/rest"
-// "github.com/zeromicro/go-zero/zrpc"
-// )
 
-// ServiceContext struct {
-//   Config     config.Config
+struct ServiceContext  {
+	config    config.Config
+	authority rest.Middleware
+	dataperm  rest.Middleware
+	redis     redis.UniversalClient
+	casbin    *casbin.Enforcer
+	trans     *i18n.Translator
+	captcha   *base64Captcha.Captcha
+}
 
-// Casbin     *casbin.Enforcer
-// Authority  rest.Middleware
-// Trans      *i18n.Translator
-// CoreRpc    coreclient.Core       //simple-adminRpc
-// EdcoresRpc edcoresclient.Edcores // EdRpc
-// }
-//
-// func NewServiceContext(c config.Config) *ServiceContext {
+fn new_service_context(c config.Config) &ServiceContext {
+	// rds := c.RedisConf.MustNewUniversalRedis()
 
-// 	rds := c.RedisConf.MustNewUniversalRedis()
+	// cbn := c.CasbinConf.MustNewCasbinWithOriginalRedisWatcher(c.DatabaseConf.Type, c.DatabaseConf.GetDSN(),
+	// 	c.RedisConf)
 
-// 	cbn := c.CasbinConf.MustNewCasbinWithOriginalRedisWatcher(c.CasbinDatabaseConf.Type, c.CasbinDatabaseConf.GetDSN(), c.RedisConf)
+	// trans := i18n.NewTranslator(c.I18nConf, i18n2.LocaleFS)
 
-// 	trans := i18n.NewTranslator(c.I18nConf, i18n2.LocaleFS)
+	// coreClient := coreclient.NewCore(zrpc.NewClientIfEnable(c.CoreRpc))
 
-// 	return &ServiceContext{
-// 		Config:     c,
-// 		Authority:  middleware.NewAuthorityMiddleware(cbn, rds, trans).Handle,
-// 		Trans:      trans,
-// 		CoreRpc:    coreclient.NewCore(zrpc.NewClientIfEnable(c.CoreRpc)),          //simple-adminRpc
-// 		EdcoresRpc: edcoresclient.NewEdcores(zrpc.NewClientIfEnable(c.EdcoresRpc)), // EdRpc
-// 	}
-// }
+	return &ServiceContext{
+		config:    c
+		// captcha:   captcha.MustNewOriginalRedisCaptcha(c.Captcha, rds),
+		// redis:     rds,
+		// casbin:    cbn,
+		// trans:     trans,
+		// authority: middleware.NewAuthorityMiddleware(cbn, rds).Handle,
+		// data_perm:  middleware.NewDataPermMiddleware(rds, coreClient, trans).Handle,
+	}
+}
