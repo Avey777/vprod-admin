2345af5cae39e7b3b33b1bf7aaf1a3518836415c
Jengro <avey777@outlook.com>
2025-05-19 14:22:09 +0800

    Update service_context.v

--- backend/internal/svc/service_context.v
+++ backend/internal/svc/service_context.v
@@ -10,7 +10,7 @@ module svc
 // "github.com/suyuan32/simple-admin-common/utils/captcha"
 // "github.com/suyuan32/simple-admin-job/jobclient"
 // "github.com/suyuan32/simple-admin-message-center/mcmsclient"
-import internal.config
+// import internal.config
 // i18n2 "github.com/suyuan32/simple-admin-core/api/internal/i18n"
 // "github.com/suyuan32/simple-admin-core/api/internal/middleware"
 // "github.com/suyuan32/simple-admin-core/rpc/coreclient"
@@ -19,33 +19,33 @@ import internal.config
 // "github.com/zeromicro/go-zero/rest"
 // "github.com/zeromicro/go-zero/zrpc"
 
-struct ServiceContext {
-	config config.Config
-	// authority rest.Middleware
-	// dataperm  rest.Middleware
-	// redis     redis.UniversalClient
-	// casbin    *casbin.Enforcer
-	// trans     *i18n.Translator
-	// captcha   *base64Captcha.Captcha
-}
+// struct ServiceContext {
+// 	config config.Config
+// authority rest.Middleware
+// dataperm  rest.Middleware
+// redis     redis.UniversalClient
+// casbin    *casbin.Enforcer
+// trans     *i18n.Translator
+// captcha   *base64Captcha.Captcha
+// }
 
-fn new_service_context(c config.Config) &ServiceContext {
-	// rds := c.RedisConf.MustNewUniversalRedis()
+// fn new_service_context(c config.Config) &ServiceContext {
+// rds := c.RedisConf.MustNewUniversalRedis()
 
-	// cbn := c.CasbinConf.MustNewCasbinWithOriginalRedisWatcher(c.DatabaseConf.Type, c.DatabaseConf.GetDSN(),
-	// 	c.RedisConf)
+// cbn := c.CasbinConf.MustNewCasbinWithOriginalRedisWatcher(c.DatabaseConf.Type, c.DatabaseConf.GetDSN(),
+// 	c.RedisConf)
 
-	// trans := i18n.NewTranslator(c.I18nConf, i18n2.LocaleFS)
+// trans := i18n.NewTranslator(c.I18nConf, i18n2.LocaleFS)
 
-	// coreClient := coreclient.NewCore(zrpc.NewClientIfEnable(c.CoreRpc))
+// coreClient := coreclient.NewCore(zrpc.NewClientIfEnable(c.CoreRpc))
 
-	return &ServiceContext{
-		config: c
-		// captcha:   captcha.MustNewOriginalRedisCaptcha(c.Captcha, rds),
-		// redis:     rds,
-		// casbin:    cbn,
-		// trans:     trans,
-		// authority: middleware.NewAuthorityMiddleware(cbn, rds).Handle,
-		// data_perm:  middleware.NewDataPermMiddleware(rds, coreClient, trans).Handle,
-	}
-}
+// return &ServiceContext{
+// 	config: c
+// captcha:   captcha.MustNewOriginalRedisCaptcha(c.Captcha, rds),
+// redis:     rds,
+// casbin:    cbn,
+// trans:     trans,
+// authority: middleware.NewAuthorityMiddleware(cbn, rds).Handle,
+// data_perm:  middleware.NewDataPermMiddleware(rds, coreClient, trans).Handle,
+// 	}
+// }
