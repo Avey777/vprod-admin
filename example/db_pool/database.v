7e751fb078d199b4735820f354d84335f16459de
Jengro <avey777@outlook.com>
2025-05-20 08:53:21 +0800

    tasks.json

--- example/db_pool/database.v
+++ example/db_pool/database.v
@@ -21,20 +21,40 @@ pub fn new_connection_pool(config pg.Config, size int) !ConnectionPool {
 	}
 }
 
-// acquire gets a connection from the pool
-pub fn (mut pool ConnectionPool) acquire() !pg.DB {
-	return <-pool.connections or { return error('Failed to acquire a connection from the pool') }
+fn main() {
+	mut pool := new_connection_pool(pg.Config{
+		host:     'localhost'
+		port:     5432
+		user:     'postgres'
+		password: '123456'
+		dbname:   'redash'
+	}, 5) or { panic('Failed to create pg pool: ${err}') }
+	dump(pool)
 }
 
-// release returns a connection back to the pool.
-pub fn (mut pool ConnectionPool) release(conn pg.DB) {
-	pool.connections <- conn
-}
+// db := pool.acquire() or { panic(err) }
+// db.exec('create table if not exists users (id serial primary key, name text not null)') or {
+// 	panic('Failed to create table users: ${err}')
+// }
+// pool.release(db)
 
-// close closes all connections in the pool.
-pub fn (mut pool ConnectionPool) close() {
-	for _ in 0 .. pool.connections.len {
-		mut conn := <-pool.connections or { break }
-		conn.close()
-	}
-}
+// pool.close()
+// }
+
+// // acquire gets a connection from the pool
+// pub fn (mut pool ConnectionPool) acquire() !pg.DB {
+// 	return <-pool.connections or { return error('Failed to acquire a connection from the pool') }
+// }
+
+// // release returns a connection back to the pool.
+// pub fn (mut pool ConnectionPool) release(conn pg.DB) {
+// 	pool.connections <- conn
+// }
+
+// // close closes all connections in the pool.
+// pub fn (mut pool ConnectionPool) close() {
+// 	for _ in 0 .. pool.connections.len {
+// 		mut conn := <-pool.connections or { break }
+// 		conn.close()
+// 	}
+// }
