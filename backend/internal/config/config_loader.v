de6e7f3b68068c87155780950a7a14a1bb9b46c0
Jengro <avey777@outlook.com>
2025-05-27 08:35:54 +0800

    Update config_loader.v

--- backend/internal/config/config_loader.v
+++ backend/internal/config/config_loader.v
@@ -99,6 +99,11 @@ pub fn (mut cl ConfigLoader) load_config() {
 		username:   doc.value('dbconf.username').string()
 		password:   doc.value('dbconf.password').string()
 		ssl_verify: doc.value('dbconf.ssl_verify').bool()
+		ssl_key:    doc.value('dbconf.ssl_key').string()
+		ssl_cert:   doc.value('dbconf.ssl_cert').string()
+		ssl_ca:     doc.value('dbconf.ssl_ca').string()
+		ssl_capath: doc.value('dbconf.ssl_capath').string()
+		ssl_cipher: doc.value('dbconf.ssl_cipher').string()
 	}
 	// 构建完整配置对象
 	cl.config = &Config{
