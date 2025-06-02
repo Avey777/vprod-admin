35211b298f6cdeaf706d45922a23b0c135e7dabf
Jengro Woo <Jengro777@outlook.com>
2025-06-02 22:37:20 +0800

    example

--- example/orm/orm_new.v
+++ example/orm/orm_new.v
@@ -5,10 +5,10 @@ import time
 import orm
 
 @[table: 'sys_users']
-struct User1 {
+struct User_2 {
 pub:
 	id         string     @[immutable; primary; sql: 'id'; sql_type: 'VARCHAR(255)'; unique]
-	name       ?string    @[immutable; sql: 'names'; sql_type: 'VARCHAR(255)'; unique]
+	name       ?string    @[immutable; sql: 'name'; sql_type: 'VARCHAR(255)'; unique]
 	created_at time.Time  @[omitempty; sql_type: 'TIMESTAMP']
 	updated_at ?time.Time @[default: new; omitempty; sql_type: 'TIMESTAMP']
 }
@@ -17,42 +17,37 @@ fn main() {
 	mut db := sqlite.connect(':memory:')!
 	defer { db.close() or {} }
 
-	user1 := User1{
-		id: '001'
-		// name:       'Jengro'
+	users1 := User_2{
+		id:         '1'
+		name:       'Jengro'
 		created_at: time.now()
 		updated_at: time.now()
 	}
 
-	user2 := User1{
-		id:         '002'
+	users2 := User_2{
+		id:         '2'
 		name:       'Dev'
 		created_at: time.now()
 		updated_at: time.now()
 	}
 
 	sql db {
-		create table User
+		create table User_2
 	} or { panic(err) }
 
-	// sql db {
-	// 	insert user1 into User
-	// 	insert user2 into User
-	// } or { panic(err) }
+	sql db {
+		insert users1 into User_2
+		insert users2 into User_2
+	} or { panic(err) }
 
 	// mut result := sql db {
 	// 	select from User
 	// } or { panic(err) }
 	// dump(result)
 
-	mut qb := orm.new_query[User1](db)
-
-	db.begin()!
-	qb.insert(user1)!
-	qb.insert(user2)!
-	db.commit()!
-
-	result1 := qb.select('id', 'names')!
+	mut qb := orm.new_query[User_2](db)
+	result1 := qb.select('id', 'name')!
+		.where('name IN ?', ['Dev'])!
 		.query()!
 	// qb.where('id != ?','000')!
 	dump(result1)
