7e45afb78ce516b7807a646b0606fa0ec2266c78
Jengro <avey777@outlook.com>
2025-06-09 15:25:40 +0800

    Delete logout_logic.go

---
+++ backend/internal/logic/sys_api/admin/user/logout_logic.v
@@ -0,0 +1,37 @@
+module user
+
+import veb
+import log
+import orm
+import x.json2
+import internal.config { db_mysql }
+import internal.structs.schema_sys
+import common.api { json_error, json_success }
+import internal.structs { Context }
+
+// Logout | 退出登入
+@['/login_out'; post]
+fn (app &User) logout_logic(mut ctx Context) veb.Result {
+	log.debug('${@METHOD}  ${@MOD}.${@FILE_LINE}')
+
+	req := json2.raw_decode(ctx.req.data) or { return ctx.json(json_error(502, '${err}')) }
+	mut result := logout_resp(mut ctx, req) or { return ctx.json(json_error(503, '${err}')) }
+
+	return ctx.json(json_success('success', result))
+}
+
+fn logout_resp(mut ctx Context, req json2.Any) !map[string]Any {
+	log.debug('${@METHOD}  ${@MOD}.${@FILE_LINE}')
+
+	mut db := db_mysql()
+	defer { db.close() }
+
+	user_id := req.as_map()['id'] or { return error('Please return user_id') }.str()
+
+	mut sys_token := orm.new_query[schema_sys.SysToken](db)
+	sys_token.set('status = ?', '1')!.where('id = ?', user_id)!.update()!
+
+	mut data := map[string]Any{}
+	data['logout'] = 'Logout successfull'
+	return data
+}
