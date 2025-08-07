32e84c1d6c7d0fc49bbfc30c75e81fc4408b1615
Jengro <avey777@outlook.com>
2025-08-07 22:30:07 +0800

    0.0.5

--- frontend/andaer-ui/nuxt.config.ts
+++ frontend/andaer-ui/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   ssr: false,
+  nitro: {
+    preset: "static",
+  },
 
   components: [{ path: "~/app/components", prefix: "App" }],
   modules: [
