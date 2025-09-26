982c72f6065868513e0dc445e13e29e65564c4db
Jengro <avey777@outlook.com>
2025-09-26 11:15:52 +0800

    09.26-3

--- backend/internal/structs/schema_sys/sys_connector.v
+++ backend/internal/structs/schema_sys/sys_connector.v
@@ -2,20 +2,20 @@ module schema_sys
 
 import time
 
-@[comment: 'Oauth提供商表/连接器表']
+// Github  Google QQ WeChat Facebook DingTalk Weibo Gitee LinkedIn Wecom Lark Gitlab Apple AzureAD Slack
+
+@[comment: '连接器表/Oauth提供商表']
 @[table: 'sys_connector']
 pub struct SysConnector {
 pub:
-	id            string @[comment: 'UUID rand.uuid_v7()'; immutable; primary; sql: 'id'; sql_type: 'CHAR(36)']
-	name          string @[comment: 'The provider`s name | 提供商名称'; omitempty; sql_type: 'VARCHAR(64)']
-	client_id     string @[comment: 'The client id | 客户端 id'; omitempty; sql_type: 'CHAR(64)']
-	client_secret string @[comment: 'The client secret | 客户端密钥'; omitempty; sql_type: 'VARCHAR(255)']
-	redirect_url  string @[comment: 'The redirect url | 跳转地址'; omitempty; sql_type: 'VARCHAR(255)']
-	scopes        string @[comment: 'The scopes | 权限范围'; omitempty; sql_type: 'VARCHAR(255)']
-	auth_url      string @[comment: 'The auth url of the provider | 认证地址'; omitempty; sql_type: 'VARCHAR(255)']
-	token_url     string @[comment: 'The token url of the provider | 获取 token地址'; omitempty; sql_type: 'VARCHAR(255)']
-	auth_style    u64    @[comment: 'The auth style, 0: auto detect 1: third party log in 2: log in with username and password | 鉴权方式 0 自动 1 第三方登录 2 使用用户名密码'; omitempty; sql_type: 'int(64)']
-	info_url      string @[comment: 'The URL to request user information by token | 用户信息请求地址'; omitempty; sql_type: 'VARCHAR(255)']
+	id          string @[comment: '连接器ID, UUID rand.uuid_v7()'; immutable; primary; sql: 'id'; sql_type: 'CHAR(36)'; unique]
+	name        string @[comment: '连接器显示名称'; required; sql_type: 'VARCHAR(100)']
+	logo        string @[comment: '连接器Logo'; omitempty; sql_type: 'VARCHAR(255)']
+	provider    string @[comment: '连接器标识,认证提供商: google, github, wechat'; required; sql_type: 'VARCHAR(100)']
+	type        u8     @[comment: '类型: 0 Email and SMS, 1 Social connectors'; required; sql_type: 'tinyint(1)']
+	config      string @[comment: '连接器配置:json格式'; required; sql_type: 'json']
+	description string @[comment: '连接器描述'; omitempty; sql_type: 'VARCHAR(500)']
+	status      u8     @[comment: '连接器状态, 0:active, 1:inactive'; default: 0; sql_type: 'tinyint(20)']
 
 	updater_id ?string    @[comment: '修改者ID'; omitempty; sql_type: 'CHAR(36)']
 	updated_at time.Time  @[comment: 'Update Time | 修改日期'; omitempty; sql_type: 'TIMESTAMP']
