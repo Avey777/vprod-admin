b5461cc8900b5be5ca18a813b0550cd3bd9de857
Jengro <avey777@outlook.com>
2025-09-29 17:30:34 +0800

    Update core_app_client.v

--- backend/internal/structs/schema_core/core_app_client.v
+++ backend/internal/structs/schema_core/core_app_client.v
@@ -10,7 +10,10 @@ pub:
 	project_id     string @[comment: 'Project ID | 项目ID'; immutable; sql: 'project_id'; sql_type: 'CHAR(36)']
 	application_id string @[comment: 'Application ID | 应用ID'; immutable; primary; sql: 'application_id'; sql_type: 'CHAR(36)']
 	name           string @[comment: 'Name | 名称'; immutable; sql: 'name'; sql_type: 'VARCHAR(255)']
-	status         u8     @[comment: '应用状态, 0:active, 1:inactive'; default: 0; sql_type: 'tinyint(20)']
+	client_type    u8     @[comment: '客户端类型：0-未知，1-WEB，2-H5，3-Android，4-iOS，5-鸿蒙，6-微信小程序'; default: 0; sql_type: 'tinyint']
+	secret         string @[comment: '客户端密钥（加密存储）'; sql_type: 'VARCHAR(255)']
+	redirect_url   string @[comment: '回调地址，多个用逗号分隔'; sql_type: 'VARCHAR(255)']
+	status         u8     @[comment: '客户端状态：0-停用，1-激活'; default: 1; sql_type: 'tinyint(1)']
 
 	updater_id ?string    @[comment: '修改者ID'; omitempty; sql_type: 'CHAR(36)']
 	updated_at time.Time  @[comment: 'Update Time | 修改日期'; omitempty; sql_type: 'TIMESTAMP']
