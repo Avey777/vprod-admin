09679f2c8f7f92aadcfdc0f8ace876bc8de34005
Jengro <avey777@outlook.com>
2025-10-08 05:21:33 +0800

    clean

--- backend/internal/structs/app_struct.v
+++ backend/internal/structs/app_struct.v
@@ -9,6 +9,12 @@ pub struct Context {
 pub mut:
 	dbpool &dbpool.DatabasePool
 	config &conf.GlobalConfig
+	// authority rest.Middleware
+	// dataperm  rest.Middleware
+	// redis     redis.UniversalClient
+	// casbin    &casbin.Enforcer
+	// trans     &i18n.Translator
+	// captcha   &base64Captcha.Captcha
 }
 
 pub struct App {
