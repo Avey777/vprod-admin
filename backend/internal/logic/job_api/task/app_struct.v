ca5110aa7b6f45fe66d6f486d51438713c093448
Jengro <avey777@outlook.com>
2025-11-02 11:27:08 +0800

    fix

--- backend/internal/logic/job_api/task/app_struct.v
+++ backend/internal/logic/job_api/task/app_struct.v
@@ -1,20 +1,6 @@
 module token
 
-import time
 import internal.structs { App }
-
-type F64 = f64
-type Any = string
-	| []string
-	| int
-	| []int
-	| []f64
-	| F64
-	| bool
-	| time.Time
-	| map[string]int
-	| []map[string]string
-	| []map[string]Any
 
 pub struct Token {
 	App
