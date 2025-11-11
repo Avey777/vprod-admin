63090399d846e3d1a97cc5b90e9b9c94401b926c
Jengro <avey777@outlook.com>
2025-11-11 14:04:58 +0800

    Update main.v

--- backend/main.v
+++ backend/main.v
@@ -6,7 +6,7 @@ fn main() {
 	banner()
 	check.check_all()!
 
-	server_thread := spawn new_app() // 启动服务器并处理错误
+	server_thread := go new_app() // 启动服务器并处理错误, http服务使用go比spawn更好
 	//* 这里主线程可以做一些其他事情 */
 	server_thread.wait() // 等待服务器线程完成并处理错误
 }
