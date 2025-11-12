182ee2841905eb8740812d981841a03f0d4fe2d6
Jengro <avey777@outlook.com>
2025-11-12 09:48:50 +0800

    optimized routes

--- backend/internal/routes/routes_core_tenant.v
+++ backend/internal/routes/routes_core_tenant.v
@@ -13,9 +13,9 @@ fn (mut app AliasApp) routes_core_tenant(conn &dbpool.DatabasePool, doc_conf &co
 
 	// 必须通过token_jwt 认证
 	app.register_routes_no_token[authentication.Authentication, structs.Context](mut &authentication.Authentication{},
-		'/core_api/core_tenant/authentication', conn, doc_conf)
-	app.register_routes_no_token[role.Role, structs.Context](mut &role.Role{}, '/core_api/core_tenant/role',
+		'/core_tenant/authentication', conn, doc_conf)
+	app.register_routes_no_token[role.Role, structs.Context](mut &role.Role{}, '/core_tenant/role',
 		conn, doc_conf)
 	app.register_routes_no_token[role_permission.RolePermission, structs.Context](mut &role_permission.RolePermission{},
-		'/core_api/core_tenant/role_permission', conn, doc_conf)
+		'/core_tenant/role_permission', conn, doc_conf)
 }
