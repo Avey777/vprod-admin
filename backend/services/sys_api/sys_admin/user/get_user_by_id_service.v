0bc82eea9327e757743f4d1138a310c5326cdf3f
Jengro <avey777@outlook.com>
2025-11-19 09:22:33 +0800

    Update get_user_by_id_service.v

--- backend/services/sys_api/sys_admin/user/get_user_by_id_service.v
+++ backend/services/sys_api/sys_admin/user/get_user_by_id_service.v
@@ -16,8 +16,8 @@ fn map_user_roles_to_ids_names(roles []SysRolePart) ([]string, []string) {
 	return role_ids, role_names
 }
 
-// 核心 Usecase
 pub fn find_user_by_id_service(mut ctx Context, mut repo UserRepository, req UserByIdReq) !UserByIdResp {
+	//
 	user_domain.find_user_by_id_domain(mut ctx, req.user_id)!
 
 	// 使用 Aggregate / UserParts
