0bd8850a9b177dc1aa732a69995736912cdb4561
Jengro <avey777@outlook.com>
2025-11-21 16:31:11 +0800

    vben5-ui

--- frontend/sys_admin-vben5-ui/packages/@core/ui-kit/shadcn-ui/src/ui/alert-dialog/AlertDialogOverlay.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-import { useScrollLock } from '@vben-core/composables';
-
-useScrollLock();
-</script>
-<template>
-  <div class="bg-overlay z-popup inset-0"></div>
-</template>
